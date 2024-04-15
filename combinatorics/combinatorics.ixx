a6502861debc7fab9c6f9fbecd2121d9fdd26f75
Alan Teixeira <alanteixeiranicacio@yahoo.com.br>
2024-04-15 17:49:40 -0300

    first combinatorics code

---
+++ combinatorics/combinatorics.ixx
@@ -0,0 +1,85 @@
+﻿export module combinatorics;
+import std;
+
+//---------------------------------------concepts-------------------------------------------
+template<typename OutR,typename T>
+concept pushing_back_range = std::ranges::output_range<OutR,std::ranges::range_value_t<OutR>>&&
+std::same_as<std::ranges::range_value_t<OutR>,T>&& std::copy_constructible<T>&&
+requires(T t, OutR r)
+{
+    {r.push_back(t)};
+};
+
+template<typename T>
+concept ostream_insertable = 
+requires(T t, std::basic_ostream<char>& oss)
+{
+    {oss<<t}->std::same_as<std::basic_ostream<char>&>;
+};
+
+//---------------------------------------auxiliar functions---------------------------------
+template<ostream_insertable T>
+std::basic_ostream<char>& operator<<(std::basic_ostream<char>& oss,const std::vector<T>& v)
+{
+    for(auto i:v)
+    {
+        oss<<i;
+    }
+    oss<<'\n';
+    return oss;
+}
+
+//---------------------------------------range based algorithms-----------------------------
+
+export template<typename T,pushing_back_range<std::vector<T>> PBR>
+void vpermutations(const std::vector<T>& vi, std::vector<T>& pref, PBR& R)
+{
+    if(vi.empty())
+    {
+        R.push_back(pref);
+    }
+    for(std::size_t i{};i<vi.size();++i)
+    {
+        std::vector<T> rem(vi.begin(),vi.begin()+i);
+        rem.insert(rem.end(),vi.begin()+i+1,vi.end());
+        pref.push_back(*(vi.begin()+i));
+        vpermutations(rem,pref, R);
+        pref.pop_back();
+    }
+}
+
+
+export template<typename T,pushing_back_range<std::vector<T>> PBR>
+void vpermutations(const std::vector<T>& vi, PBR& R)
+{
+    std::vector<T> v;
+    v.reserve(vi.size());
+    vpermutations(vi,v, R);
+}
+//---------------------------------------stream based algorithms----------------------------
+export template<std::copy_constructible T>
+requires std::destructible<T>
+void vpermutations(const std::vector<T>& vi, std::vector<T>& pref, std::basic_ostream<char>& oss)
+{
+    if(vi.empty())
+    {
+        oss<<pref;
+    }
+    for(std::size_t i{};i<vi.size();++i)
+    {
+        std::vector<T> rem(vi.begin(),vi.begin()+i);
+        rem.insert(rem.end(),vi.begin()+i+1,vi.end());
+        pref.push_back(*(vi.begin()+i));
+        vpermutations(rem,pref, oss);
+        pref.pop_back();
+    }
+}
+
+
+export template<std::copy_constructible T>
+void vpermutations(const std::vector<T>& vi, std::basic_ostream<char>& oss)
+{
+    std::vector<T> v;
+    v.reserve(vi.size());
+    vpermutations(vi,v, oss);
+}
---
+++ combinatorics/test.cpp
@@ -0,0 +1,101 @@
+﻿#include <ranges>
+#include <concepts>
+#include <vector>
+#include <iostream>
+#include <span>
+
+
+//-----------------------------------------range_base------------------------------------------------
+template<typename OutR,typename T>
+concept pushing_back_range = std::ranges::output_range<OutR,std::ranges::range_value_t<OutR>>&&
+std::same_as<std::ranges::range_value_t<OutR>,T>&& std::copy_constructible<T>&&
+requires(T t, OutR r)
+{
+    {r.push_back(t)};
+};
+
+template<typename T,pushing_back_range<std::vector<T>> PBR>
+void vpermutations(const std::vector<T>& vi, std::vector<T>& pref, PBR& R)
+{
+    if(vi.empty())
+    {
+        R.push_back(pref);
+    }
+    for(std::size_t i{};i<vi.size();++i)
+    {
+        std::vector<T> rem(vi.begin(),vi.begin()+i);
+        rem.insert(rem.end(),vi.begin()+i+1,vi.end());
+        pref.push_back(*(vi.begin()+i));
+        vpermutations(rem,pref, R);
+        pref.pop_back();
+    }
+}
+
+template<typename T,pushing_back_range<std::vector<T>> PBR>
+void vpermutations(const std::vector<T>& vi, PBR& R)
+{
+    std::vector<T> v;
+    v.reserve(vi.size());
+    vpermutations(vi,v, R);
+}
+
+//----------------------------------------stream_base---------------------------------------------------
+template<typename T>
+concept ostream_insertable = 
+requires(T t, std::basic_ostream<char>& oss)
+{
+    {oss<<t}->std::same_as<std::basic_ostream<char>&>;
+};
+
+template<ostream_insertable T>
+std::basic_ostream<char>& operator<<(std::basic_ostream<char>& oss,const std::vector<T>& v)
+{
+    for(auto a :v)
+    {
+        oss<<a;
+    }
+    oss<<'\n';
+    return oss;
+}
+
+template<std::copy_constructible T>
+requires std::destructible<T>
+void vpermutations(const std::vector<T>& vi, std::vector<T>& pref, std::basic_ostream<char>& oss)
+{
+    if(vi.empty())
+    {
+        oss<<pref;
+    }
+    for(std::size_t i{};i<vi.size();++i)
+    {
+        std::vector<T> rem(vi.begin(),vi.begin()+i);
+        rem.insert(rem.end(),vi.begin()+i+1,vi.end());
+        pref.push_back(*(vi.begin()+i));
+        vpermutations(rem,pref, oss);
+        pref.pop_back();
+    }
+}
+
+template<std::copy_constructible T>
+void vpermutations(const std::vector<T>& vi, std::basic_ostream<char>& oss)
+{
+    std::vector<T> v;
+    v.reserve(vi.size());
+    vpermutations(vi,v, oss);
+}
+
+
+int main()
+{
+    std::vector v{1,2,3};
+    std::vector<std::vector<int>> vv;
+    vpermutations(v,vv);
+    for(auto v:vv)
+    {
+        for(auto i:v)
+        {
+            std::cout<<i;
+        }
+        std::cout<<'\n';
+    }
+}

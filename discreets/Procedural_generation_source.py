3f0d7878d4f81936d6aa1eefe1fec711c4259b32
Alan Teixeira <alanteixeiranicacio@yahoo.com.br>
2024-05-06 14:48:03 -0300

    "Improvement of explanation in comments"

--- discreets/Procedural_generation_source.py
+++ discreets/Procedural_generation_source.py
@@ -12,28 +12,28 @@ import turtle as trt
 #--------------------------Weierstrass function--------------------------------
 
 # dpoints is the array of domain points evaluated and npartial
-# is the number of partial sum considered
+# is the number of terms in the partial sum
 def weierstrass(dpoints,npartial):
     image=np.zeros(dpoints.size)
     for n in range(0,npartial):
         image=image+np.cos(3**n*np.pi*dpoints)/2**n
     return image
 
+# this function draws the curve
 def draw_weierstrass():
 # x axis main interval, start and end points
     start=-1
     stop=1
 # number of subintervals of main interval
     intervals = 10000
-# domain points
+# domain points where the function will be evaluated
     xpoints = np.arange(start,stop,((stop-start)/intervals))
 # print the function graph
     plt.plot(xpoints,weierstrass(xpoints, 600))
     plt.show()
 
-#draw_weierstrass()
 
-#----------------------------fibonacci word curve-------------------------------
+#----------------------------Fibonacci word curve-------------------------------
 
 # @cache is for memoization. This function constructs the nth Fibonacci word
 @cache
@@ -47,22 +47,25 @@ def fibonacci_word(n):
     return fibonacci_word(n - 1) + fibonacci_word(n - 2)
 
 # draw the curve using turtle graphics
-def draw_fibonacci_curve(word, step):
+# word is the Fibonacci word, leng is the legth of the segment used in drawing
+def draw_fibonacci_curve(word, leng):
     for i, c in enumerate(word, 1):
-        trt.forward(step)
+        trt.forward(leng)
         if c == "0":
             if i % 2 == 0:
                 trt.left(90)
             else:
                 trt.right(90)
 
+# this function configures and sets up a turtle screen and call the curve drawer 
+# call it to draw the curve
 def launch_turtle_fibonacci():
     n = 21 # the nth fibonacci word.
-    step = 1 # Segment length in pixels (1 since it will be hundreds of them).
+    leng = 1 # Segment length in pixels (1 since there will be hundreds of them).
     width = 1050 # Width of plot area in pixels.
     height = 750 # Height of plot area in pixels.
     w = fibonacci_word(n)
-
+# for more information about turtle methods consult the module's documentation
     trt.setup(width=width, height=height,starty=1)
     trt.speed(0) # no lines drawing animation
     trt.setheading(90)
@@ -72,20 +75,20 @@ def launch_turtle_fibonacci():
     trt.right(90)
     trt.backward(300)
     trt.pendown()
-    trt.tracer(1000) # draw appears on screen only after each 
-                     # 1000 screen updates
+    trt.tracer(1000) # draw appears on screen only after each 1000 screen updates
+                     
     trt.hideturtle() # hide the turtle while drawing
-    draw_fibonacci_curve(w, step)
+    draw_fibonacci_curve(w, leng)
     trt.exitonclick()
 
-#launch_turtle_fibonacci()
 
 #---------------------------------binary tree-----------------------------------
 
 # constructs the l-system for the binary tree
-def binarytreesystem(steps):
-    lsys='0'
-    while steps>0:
+# iterations is the number of iterations to be executed
+def binarytreesystem(iterations):
+    lsys='0' # the initial string
+    while iterations>0:
         sz=len(lsys)
         i=0
         c=1
@@ -101,10 +104,12 @@ def binarytreesystem(steps):
             elif lsys[i]=='[' or lsys[i]==']':
                 i+=1
                 c+=1
-        steps-=1
+        iterations-=1
     return lsys
 
-# draw the binary tree. lsys is the l-sytem to be drawn
+# this function draws the binary tree 
+# lsys is the l-sytem to be drawn
+# branchlen, leaflen are the lengths of branchs and leafs
 def drawtree(lsys,branchlen,leaflen):
     stack=[]
     trt.setheading(90)
@@ -124,11 +129,12 @@ def drawtree(lsys,branchlen,leaflen):
             trt.right(45)
             trt.pendown()
 
-# launch turtle graphics
+# this function sets up a turtle screen and launch turtle graphics
+# call it to draw the the tree
 def launch_turtle_binary():
     blen = 5 # branchs length
     llen = 2 # leafs length
-    n = 7 # steps
+    n = 7 # number of iterations
     width = 1050 # Width of plot area in pixels.
     height = 750 # Height of plot area in pixels.
     sys=binarytreesystem(n) # binary tree l-system
@@ -144,14 +150,14 @@ def launch_turtle_binary():
     drawtree(sys,5,2)
     trt.exitonclick()
 
-#launch_turtle_binary()
 
-#---------------------------------cantor set------------------------------------
+#---------------------------------Cantor set------------------------------------
 
-# constructs the cantor set l-system
-def cantorsetsystem(steps):
-    lsys='A'
-    while steps>1:
+# constructs one step of the cantor set l-system
+# step of modification of the original segment
+def cantorsetsystem(step):
+    lsys='A' # initial string
+    while step>1:
         sz=len(lsys)
         i=0
         c=1
@@ -164,14 +170,17 @@ def cantorsetsystem(steps):
                 lsys=lsys[:i]+'BBB'+lsys[i+1:]
                 i+=3
                 c+=1
-        steps-=1
+        step-=1
     return lsys
 
-# draw cantor set. origlen is the length of the original segment
-def drawcantorset(steps,origlen):
+# draw cantor set. 
+# origlen is the length of the original segment
+# iterartions is the number of iterations to be executed
+def drawcantorset(iteartions,origlen):
     linesize=origlen
-    for i in range(1,steps+1):
-        lsys=cantorsetsystem(i)
+    for i in range(1,iteartions+1):
+        lsys=cantorsetsystem(i) # each iteration is a step in the modification of the original segment
+                                # so each iteration creates a new l-system
         for s in lsys:
             if s=='A':
                 trt.forward(linesize)
@@ -188,8 +197,10 @@ def drawcantorset(steps,origlen):
         trt.pendown()
         linesize/=3
     
+# this function sets up and launchs a turtle graphics
+# call it to drawn the Cantor set
 def launch_turtle_cantor():
-    n = 5 # steps
+    n = 5 # number of iterations
     linelength=600 # original segment length
     width = 1050 # Width of plot area in pixels.
     height = 750 # Height of plot area in pixels.
@@ -204,5 +215,3 @@ def launch_turtle_cantor():
     trt.hideturtle()
     drawcantorset(n,linelength)
     trt.exitonclick()
-
-#launch_turtle_cantor()
